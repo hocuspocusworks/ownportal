0e71d5254c937263317c434076d049152fa6a667
Jean Petric <petricomp@gmail.com>
2022-03-21 22:25:08 +0000

    converted fetcher into webflux

--- fetcher/src/main/java/net/ownportal/fetcher/FetchController.java
+++ fetcher/src/main/java/net/ownportal/fetcher/FetchController.java
@@ -1,33 +1,52 @@
 package net.ownportal.fetcher;
 
-import java.io.IOException;
 import java.net.URL;
 
-import org.apache.commons.io.IOUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import net.ownportal.RssPage;
 import net.ownportal.RssResolver;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 @RestController
 @RequestMapping("/rss")
 class FetchController {
+    private final WebClient webClient;
+
+    FetchController(WebClient.Builder builder) {
+        this.webClient = builder.build();
+    }
+
     @PostMapping("/validate")
-    public boolean validateRss(final String url) throws IOException {
+    public Mono<Boolean> validateRss(final String url) throws Exception {
         URL page = new URL(url);
-        byte[] data = IOUtils.toByteArray(page);
-        RssResolver rss = new RssResolver();
-        return rss.isValid(data);
+        return webClient.get()
+            .uri(page.toURI())
+            .retrieve()
+            .bodyToMono(byte[].class)
+            .publishOn(Schedulers.boundedElastic())
+            .map(data -> {
+                RssResolver rss = new RssResolver();
+                return rss.isValid(data);
+            });
     }
 
     @GetMapping("/fetch")
-    public RssPage fetchRss(final String url) throws IOException {
+    public Mono<RssPage> fetchRss(final String url) throws Exception {
         URL page = new URL(url);
-        byte[] data = IOUtils.toByteArray(page);
-        RssResolver rss = new RssResolver();
-        return rss.rssToJson(data);
+        return webClient.get()
+            .uri(page.toURI())
+            .retrieve()
+            .bodyToMono(byte[].class)
+            .publishOn(Schedulers.boundedElastic())
+            .map(data -> {
+                RssResolver rss = new RssResolver();
+                return rss.rssToJson(data);
+            });
     }
 }
