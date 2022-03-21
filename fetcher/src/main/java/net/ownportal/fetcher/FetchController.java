55ff37c33394e820ada9713f73bcfe57c7ebb2a0
Jean Petric <petricomp@gmail.com>
2022-03-21 21:56:08 +0000

    adding validation endpoint

--- fetcher/src/main/java/net/ownportal/fetcher/FetchController.java
+++ fetcher/src/main/java/net/ownportal/fetcher/FetchController.java
@@ -1,9 +1,11 @@
 package net.ownportal.fetcher;
 
+import java.io.IOException;
 import java.net.URL;
 
 import org.apache.commons.io.IOUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,12 +15,19 @@ import net.ownportal.RssResolver;
 @RestController
 @RequestMapping("/rss")
 class FetchController {
-    @GetMapping("/fetch")
-    public RssPage fetchRss(final String url) throws Exception {
+    @PostMapping("/validate")
+    public boolean validateRss(final String url) throws IOException {
         URL page = new URL(url);
         byte[] data = IOUtils.toByteArray(page);
         RssResolver rss = new RssResolver();
-        RssPage json = rss.rssToJson(data);
-        return json;
+        return rss.isValid(data);
+    }
+
+    @GetMapping("/fetch")
+    public RssPage fetchRss(final String url) throws IOException {
+        URL page = new URL(url);
+        byte[] data = IOUtils.toByteArray(page);
+        RssResolver rss = new RssResolver();
+        return rss.rssToJson(data);
     }
 }
