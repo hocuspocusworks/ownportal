a73d3facd83a37cca752940fe332c2754bff9e1b
Jean Petric <petricomp@gmail.com>
2022-03-25 21:04:29 +0000

    comments on non blocking gateway

--- gateway/src/main/java/net/ownportal/gateway/AuthFilter.java
+++ gateway/src/main/java/net/ownportal/gateway/AuthFilter.java
@@ -9,6 +9,17 @@ import org.springframework.web.server.ServerWebExchange;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 
+// https://github.com/spring-cloud/spring-cloud-gateway/issues/1090
+// @Override
+// public GatewayFilter apply(Config config) {
+//     return ((exchange, chain) -> {
+//         return WebClient.create().post().uri("http://127.0.0.1:9001/user/query").retrieve().bodyToMono
+//                 (String.class).flatMap(s -> {
+//             System.out.println(s);
+//             return chain.filter(exchange);
+//         });
+//     });
+
 @Component
 @Slf4j
 public class AuthFilter implements GlobalFilter, Ordered {
