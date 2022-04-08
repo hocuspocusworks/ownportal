1f0071a73e38828c1044756191b65ccf617256a8
Jean Petric <petricomp@gmail.com>
2022-04-08 21:33:22 +0100

    UI minor improvements

--- webapp/src/components/MainContent.vue
+++ webapp/src/components/MainContent.vue
@@ -4,17 +4,17 @@
         <Message v-if="err" severity="error" closable="false" class="col-12">Cannot load the feed.</Message>
 
         <div v-for="(item,i) in content" :key="i" class="p-2 sm:col-12 lg:col-4">
-        <div class="min-h-full border-1 border-round border-bluegray-50" style="height: 300px;">
+        <div class="min-h-full border-1 border-round border-bluegray-50 text-600 no-underline" style="height: 300px;">
                 <Card>
                     <template #title>
-                        <a style="text-decoration: none; color: black;" target="_blank" :href="item.link">{{ item.title }}</a>
+                        <a class="text-800 no-underline" target="_blank" :href="item.link">{{ item.title }}</a>
                     </template>
                     <template #subtitle>
                         <small>{{ item.source }}</small>
                     </template>
                     <template class="flex-grow-1" #content>
-                        <a v-if="!item.description" style="text-decoration: none; color: black;" target="_blank" :href="item.link">Missing content...</a>
-                        <a style="text-decoration: none; color: black;" target="_blank" :href="item.link">{{ item.description }}</a>
+                        <a v-if="!item.description" class="text-600 no-underline"  target="_blank" :href="item.link">Missing content...</a>
+                        <a class="text-600 no-underline" target="_blank" :href="item.link">{{ item.description }}</a>
                     </template>
                     <template #footer>
                         <div class="flex">
