5ed5112b755022788ce83ae4bff7df52befa258f
Jean Petric <petricomp@gmail.com>
2022-04-10 20:32:34 +0100

    refactored explore to use bootstrap

--- webapp/src/views/Explore.vue
+++ webapp/src/views/Explore.vue
@@ -39,9 +39,24 @@
                         </div>
                     </div>
 
-                    <div v-if="!featured" class="row p-0 m-0">
-                        <div class="card" v-for="(item, i) in sources" :key="i">
-                            {{item.name}}
+                    <div v-if="!featured" class="p-0 m-0">
+                        <div class="p-4" style="height: 8rem;" v-for="(item, i) in sources" :key="i">
+                            <div class="row border rounded gx-0">
+                                <div class="col-12 text-start gx-3 fs-4">
+                                    {{ item.name }}
+                                </div>
+                                <div class="col-12 text-start gx-3">
+                                    {{ item.description }}
+                                </div>
+                                <div class="col-12 gx-3">
+                                    <div class="input-group justify-content-end">
+                                        <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Add to a Group</button>
+                                        <ul class="dropdown-menu">
+                                            <li><a class="dropdown-item" href="#" v-for="(grp, j) in groups" @click="addToGroup(grp.name, item.name, item.url)">{{ grp.name }}</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -50,101 +65,20 @@
             <div class="col-sm-0 col-lg-2"></div>
         </div>
     </div>
-    <!--
-    <div class="grid">
-        <div class="col-8 lg:col-offset-2">
-            <TabView>
-                <TabPanel header="Feeds">
-                    <div class="col-12 text-600">
-                        <div class="flex">
-                            <div class="flex-initial flex">
-                                <p class="text-2xl">Discover new feeds and articles</p>
-                            </div>
-                        </div>
-                        <div class="flex">
-                            <span class="flex flex-grow-1 p-input-icon-left">
-                                <i class="pi pi-search" />
-                                <InputText v-model="search" type="text" class="inputfield w-full" placeholder="Start typing some subject or insert an RSS link" />
-                            </span>
-                        </div>
-                        <div class="mt-4">
-                            <div v-if="featured" class="grid">
-                                <div class="col-12">
-                                    <div class="flex text-xs font-semibold">TRENDING</div>
-                                </div>
-                                <div class="grid col-12">
-                                    <div class="col-3" v-for="(item, i) in trending" :key="i">
-                                        <div class="card border-round surface-200 h-14rem flex justify-content-center">
-                                            <div class="flex align-items-center">{{ item.name }}</div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="col-12 mt-2">
-                                    <div class="flex text-xs font-semibold">CATEGORIES</div>
-                                </div>
-                                <div class="grid col-12">
-                                    <div class="col-3" v-for="(item, i) in categories" :key="i">
-                                        <div class="card border-round surface-200 h-6rem flex justify-content-center">
-                                            <div class="flex align-items-center">{{ item.name }}</div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div v-if="!featured">
-                                <div class="card border-round surface-200 h-12rem justify-content-center" v-for="(item, i) in sources" :key="i">
-                                    <div class="flex flex-grow-1 pl-3 pt-3">
-                                        <div class="text-left text-2xl">{{ item.name }}</div>
-                                        <div class="text-right text-sm">{{ item.language }}</div>
-                                    </div>
-                                    <div class="flex pl-3">
-                                        <div class="text-left">{{ item.url }}</div>
-                                    </div>
-                                    <div class="flex pl-3 pt-3">
-                                        <div class="text-left">{{ item.description }}</div>
-                                    </div>
-                                    <div class="flex pl-3 pt-5">
-                                        <div class="text-left">
-                                            <Dropdown v-model="selectedGroup" :options="groups" optionLabel="name" placeholder="Add to a Group" @click="streamSelection(item.name, item.url)" />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </TabPanel>
-            </TabView>
-        </div>
-        <div class="lg:col-2"></div>
-    </div>
-    -->
 </template>
 
 <script>
 import axios from 'axios';
 import config from '../config';
-import TabView from 'primevue/tabview';
-import TabPanel from 'primevue/tabpanel';
-import InputText from 'primevue/inputtext';
-import Dropdown from 'primevue/dropdown';
 
 export default {
     name: "Explore",
-    components: {
-        TabView,
-        TabPanel,
-        InputText,
-        Dropdown
-    },
     data() {
         return {
             search: "",
             featured: true,
             sources: [],
             groups: [],
-            selectedGroup: "",
-            selectedStream: "",
-            selectedName: "",
             trending: ["t1", "t2", "t3", "t4"],
             categories: ["a", "b", "c", "d"],
             items: [
@@ -169,13 +103,9 @@ export default {
                     this.groups = response.data.groups;
                 });
         },
-        streamSelection(name, url) {
-            this.selectedName = name;
-            this.selectedStream = url;
-        },
-        addToGroup() {
+        addToGroup(grpName, name, uri) {
             let url = config.gateway + "/portal/feed/newStream";
-            let payload = {group: this.selectedGroup.name, stream: this.selectedName, url: this.selectedStream};
+            let payload = {group: grpName, stream: name, url: uri};
             axios.post(url, payload, {withCredentials: true})
                 .then(response => {
                     if (response.status === 200) {
@@ -208,9 +138,6 @@ export default {
                         }
                     });
             }
-        },
-        selectedGroup: function(newValue, oldValue) {
-            this.addToGroup(newValue);
         }
     },
     mounted() {
