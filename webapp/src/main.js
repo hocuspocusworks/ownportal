c51bf62205cd475955ad58c7bb6452899b632583
Jean Petric <petricomp@gmail.com>
2022-04-11 22:58:12 +0100

    UI full removal of primevue

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -1,19 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import PrimeVue from 'primevue/config';
 import router from './router/index.js';
 import "./index.css";
 
-// import 'primevue/resources/themes/saga-blue/theme.css';
-import 'primevue/resources/themes/lara-light-blue/theme.css';
-import 'primevue/resources/primevue.min.css';
-import 'primeicons/primeicons.css';
-import 'primeflex/primeflex.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap";
 
 const app = createApp(App);
 app.use(router);
-app.use(PrimeVue);
 app.mount("#app");
