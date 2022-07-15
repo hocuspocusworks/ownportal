b2563c4454f8530bdb965ae768fd9bb4466c2fea
Jean Petric <petricomp@gmail.com>
2022-07-15 11:09:48 +0200

    importing the right file for vue hash history

--- webapp/src/router/index.js
+++ webapp/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import Home from '../views/Home.vue';
 import Explore from '../views/Explore.vue';
 import MainContent from '../components/MainContent.vue';
