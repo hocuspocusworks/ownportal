8e44da03f0541add4f707ad062e15f6170453280
Jean Petric <petricomp@gmail.com>
2022-07-15 11:13:45 +0200

    rollback of the vue router

--- webapp/src/router/index.js
+++ webapp/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Home from '../views/Home.vue';
 import Explore from '../views/Explore.vue';
 import MainContent from '../components/MainContent.vue';
@@ -40,7 +40,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes: routes,
 });
 
