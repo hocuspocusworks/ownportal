3cb23e5458976dc92cf134d5985857698965f256
Jean Petric <petricomp@gmail.com>
2022-09-16 21:46:12 +0200

    Refactor session controller

--- api/app/controllers/api/sessions_controller.rb
+++ api/app/controllers/api/sessions_controller.rb
@@ -1,12 +1,14 @@
 module Api
   class SessionsController < ApplicationController
+    include Api::Extensions::Respondable
+
     skip_before_action :authenticate
 
     def create
       user = User.find_by(email: params[:session][:email])
 
       if user.present? && user.authenticate(params[:session][:password])
-        render json: SessionSerializer.render(user, root: :session), status: :ok
+        render_json user
       else
         render json: { errors: { credentials: ['are invalid'] } },
                status: :unauthorized
