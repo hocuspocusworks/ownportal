a13494148ff0a08abd87c6529bdb4dbf73c20620
Jean Petric <petricomp@gmail.com>
2022-09-18 15:56:49 +0200

    Minor change

--- api/app/models/favourite.rb
+++ api/app/models/favourite.rb
@@ -13,14 +13,6 @@
 #  updated_at     :datetime         not null
 #
 class Favourite < ApplicationRecord
-  after_save :save_tags
-
   has_many :taggings
   belongs_to :user
-
-  private
-
-  def save_tags
-    # noop
-  end
 end
