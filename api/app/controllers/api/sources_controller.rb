d6e76d5aebf8309e4ae6461d4f228ffc9f93f527
Jean Petric <petricomp@gmail.com>
2022-09-24 23:50:32 +0200

    Source controller fixed

--- api/app/controllers/api/sources_controller.rb
+++ api/app/controllers/api/sources_controller.rb
@@ -1,20 +1,20 @@
 module Api
   class SourcesController < ApplicationController
+    include Api::Extensions::Resourceful
+
     def index
       render json: Source.first(20)
     end
 
     def create
-      source = Source.create(user_params)
-
-      if source.save
-        render json: source, status: :created
-      else
-        render json: { errors: source.errors }, status: :bad_request
-      end
+      save_form
     end
 
     private
+
+    def policy_class
+      SourcePolicy
+    end
 
     def load_resource
       @source ||= Source.find(params[:id])
--- api/app/policies/source_policy.rb
+++ api/app/policies/source_policy.rb
@@ -1,5 +1,9 @@
 # Pundit is resource-based, not controller-based. When you call authorize and pass it a resource, Pundit cares about the action name and the resource type, but it does not care about the controller name.
 class SourcePolicy < ApplicationPolicy
+  def create?
+    true
+  end
+
   def update?
     admin?
   end
