e146322cc9ac0969768a12edc3fe7a83ae805d30
Jean Petric <petricomp@gmail.com>
2022-09-25 21:39:20 +0200

    Favourite published date fixed

--- api/app/models/concerns/favourite_deserialiser.rb
+++ api/app/models/concerns/favourite_deserialiser.rb
@@ -5,6 +5,7 @@ class FavouriteDeserialiser
     favourite.link = params[:link] || resource&.link
     favourite.publisher = params[:publisher] || resource&.publisher
     favourite.title = params[:title] || resource&.title
+    favourite.published_date = params[:published_date] || resource&.published_date
     favourite.user = user
     favourite.tags = Tag.find_or_create(params[:tag_list], user)
     favourite.tags_json = params[:tag_list] || resource&.tags_json
