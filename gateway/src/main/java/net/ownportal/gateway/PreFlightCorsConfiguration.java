aa1032517f71de40e0704f7103234297db00a6e6
Jean Petric <petricomp@gmail.com>
2022-09-25 18:42:46 +0200

    Sort out preflight headers

--- gateway/src/main/java/net/ownportal/gateway/PreFlightCorsConfiguration.java
+++ gateway/src/main/java/net/ownportal/gateway/PreFlightCorsConfiguration.java
@@ -17,7 +17,7 @@ import reactor.core.publisher.Mono;
 @Configuration
 public class PreFlightCorsConfiguration {
 
-    private static final String ALLOWED_HEADERS = "Authorization";
+    private static final String ALLOWED_HEADERS = "Content-Type, Authorization";
     private static final String ALLOWED_METHODS = "GET, PUT, POST, DELETE, OPTIONS, PATCH";
     private static final String ALLOWED_ORIGIN = "*";
     private static final String MAX_AGE = "7200";
