873eb4cbdd481ce8bfac51c615bbb576e1dab357
Jean Petric <petricomp@gmail.com>
2022-10-03 20:48:11 +0200

    Fix admin menu

--- webapp/src/views/Admin.vue
+++ webapp/src/views/Admin.vue
@@ -1,22 +1,27 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Admin</a>
-
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="#" @click="categories">Categories</a>
-        </li>
-        <li class="nav-item active">
-          <a class="nav-link" href="#" @click="sources('admin')">Admin Sources</a>
-        </li>
-        <li class="nav-item active">
-          <a class="nav-link" href="#" @click="sources('contributor')">Contributor Sources</a>
-        </li>
-        <li class="nav-item active">
-          <a class="nav-link" href="#" @click="stats()">Stats</a>
-        </li>
-      </ul>
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">Admin</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="#" @click="categories">Categories</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#" @click="sources('admin')">Admin Sources</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#" @click="sources('contributor')">Contributor Sources</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="#" @click="stats()">Stats</a>
+          </li>
+        </ul>
+      </div>
     </div>
   </nav>
   <router-view></router-view>
@@ -29,13 +34,13 @@ export default {
   name: "Admin",
   methods: {
     categories() {
-      router.push({name: 'categories'})
+      router.push({ name: 'categories' })
     },
     sources(kind) {
-      router.push({name: 'sources', params: {kind: kind}})
+      router.push({ name: 'sources', params: { kind: kind } })
     },
     stats() {
-      router.push({name: 'stats'})
+      router.push({ name: 'stats' })
     }
   }
 }
