6d44a624846c7328b96a2ecc52c0196a4a4f775a
Jean Petric <petricomp@gmail.com>
2022-10-08 00:10:54 +0200

    Update annotations

--- api/app/models/explore.rb
+++ api/app/models/explore.rb
@@ -3,7 +3,7 @@
 # Table name: sources
 #
 #  id          :bigint           not null, primary key
-#  description :string
+#  description :string(512)
 #  icon        :string
 #  language    :string
 #  name        :string           not null
@@ -11,12 +11,13 @@
 #  url         :string           not null
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
-#  processed   :boolean
+#  processed   :boolean          default(FALSE)
 #  categories  :jsonb
 #  published   :boolean
 #  restricted  :boolean          default(TRUE), not null
 #  creator_id  :bigint
 #  counter     :integer
+#  visibility  :integer          default(0)
 #
 class Explore < ApplicationRecord
   self.table_name = 'sources'
