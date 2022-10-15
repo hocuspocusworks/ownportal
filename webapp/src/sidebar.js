59d08aa9f67fbf1f338ba8f69c2a58c616baca2c
Jean Petric <petricomp@gmail.com>
2022-10-15 08:23:24 +0200

    Fixed sidebar

--- webapp/src/sidebar.js
+++ webapp/src/sidebar.js
@@ -9,29 +9,33 @@ const sidebar = {
         }
 
         if (this.sidebarOpen) {
-            this.closeNav();
-            this.sidebarOpen = false;
+            this.closeNav()
+            this.sidebarOpen = false
         } else {
-            this.openNav();
-            this.sidebarOpen = true;
+            this.openNav()
+            this.sidebarOpen = true
         }
     },
     closeNav: function() {
         if (window.innerWidth <= this.cutoffWidth) {
-            document.getElementById("mySidebar").setAttribute('class', 'd-none')
-            document.getElementById("myContent").setAttribute('class', 'w-100 bg-dark-middle')
+            document.getElementById("mySidebar").classList.add('d-none')
+            document.getElementById("myContent").classList.add('w-100')
+            document.getElementById("mySidebar").classList.remove('w-100')
+            document.getElementById("myContent").classList.remove('d-none')
         } else {
-            document.getElementById("mySidebar").style.minWidth = "0";
-            document.getElementById("mySidebar").style.maxWidth = "0";
+            document.getElementById("mySidebar").style.minWidth = "0"
+            document.getElementById("mySidebar").style.maxWidth = "0"
         }
     },
     openNav: function() {
         if (window.innerWidth <= this.cutoffWidth) {
-            document.getElementById("mySidebar").setAttribute('class', 'w-100 text-white bg-dark-strong')
-            document.getElementById("myContent").setAttribute('class', 'd-none')
+            document.getElementById("mySidebar").classList.remove('d-none')
+            document.getElementById("myContent").classList.remove('w-100')
+            document.getElementById("mySidebar").classList.add('w-100')
+            document.getElementById("myContent").classList.add('d-none')
         } else {
-            document.getElementById("mySidebar").style.minWidth = "320px";
-            document.getElementById("mySidebar").style.maxWidth = "320px";
+            document.getElementById("mySidebar").style.minWidth = "320px"
+            document.getElementById("mySidebar").style.maxWidth = "320px"
         }
     },
     activated: function() {
