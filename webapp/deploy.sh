1787d0befd289b9aa1085bd710d8aa0f1f5e3e02
Jean Petric <petricomp@gmail.com>
2022-10-18 23:32:55 +0200

    FE Fix deploy script for new server setup

--- webapp/deploy.sh
+++ webapp/deploy.sh
@@ -3,10 +3,10 @@
 cp ./src/.server.prod.js ./src/.server.js
 yarn build
 zip -r dist.zip dist
-scp dist.zip rack:/html/ownportal/.
-ssh rack "rm -fr /html/ownportal/dist"
-ssh rack "unzip /html/ownportal/dist.zip -d /html/ownportal"
-ssh rack "rm /html/ownportal/dist.zip"
+scp dist.zip ownportal:/home/ownportal/.
+ssh ownportal "rm -fr /home/ownportal/dist"
+ssh ownportal "unzip /home/ownportal/dist.zip -d /home/ownportal"
+ssh ownportal "rm /home/ownportal/dist.zip"
 
 rm dist.zip
 cp ./src/.server.dev.js ./src/.server.js
