d2cedfaaf7048eaabc7a44d012911132a7572e2d
Jean Petric <petricomp@gmail.com>
2022-10-23 18:57:55 +0200

    Implement simple throttle

--- api/app/controllers/application_controller.rb
+++ api/app/controllers/application_controller.rb
@@ -3,15 +3,39 @@ class ApplicationController < ActionController::API
   include Api::Extensions::Authentication
   include Api::Extensions::Respondable
 
+  before_action :throttle
   before_action :authenticate
   before_action :load_resource, only: %i[show update destroy]
   after_action :verify_authorized
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  rescue_from Exceptions::TooManyRequests, with: :too_many_requests_error
 
   private
 
   def user_not_authorized
     render json: { 'errors': { 'token': ['is invalid'] } }, status: :forbidden
+  end
+
+  def throttle
+    raise Exceptions::TooManyRequests if requests_count > requests_limit
+
+    next_request_count
+  end
+
+  def next_request_count
+    Rails.cache.write(request.remote_ip, requests_count + 1, expires_in: 1.minute)
+  end
+
+  def requests_count
+    Rails.cache.fetch(request.remote_ip) { 1 }
+  end
+
+  def requests_limit
+    Rails.cache.fetch('max_requests_per_minute') { 200 }
+  end
+
+  def too_many_requests_error
+    render json: { 'errors': { 'throttle': 'request limit reached' } }, status: :too_many_requests
   end
 end
---
+++ api/lib/exceptions.rb
@@ -0,0 +1,3 @@
+module Exceptions
+  class TooManyRequests < StandardError; end
+end
