74741682bd916c451594b6ddb4b7ca593510d8db
Jean Petric <petricomp@gmail.com>
2022-11-13 13:51:15 +0100

    FE Redirect to sidekiq correctly

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -10,7 +10,9 @@ export default {
   name: 'App',
   mounted() {
     config.setScreenSize()
-    if (window.location.pathname.startsWith('/admin')) {
+    if (window.location.pathname.startsWith('/sidekiq')) {
+      window.location.href = config.gateway + "/sidekiq";
+    } else if (window.location.pathname.startsWith('/admin')) {
       router.push({name: 'admin'})
     } else if (window.location.pathname.startsWith('/creator')) {
       router.push({name: 'creator'})
