3da1149540d2082809bbb78ba4777bfbdf2f267b
Jean Petric <petricomp@gmail.com>
2022-12-06 22:02:29 +0100

    FE Adjust settings for dark mode

--- webapp/src/views/Setting.vue
+++ webapp/src/views/Setting.vue
@@ -3,15 +3,15 @@
     <div class="row pt-2 p-lg-4">
       <div class="col-lg-2"></div>
       <div class="col-12 col-lg-8">
-        <div class="card">
+        <div class="card" :class="themeCard">
           <div class="card-body text-black">
-            <h5 class="card-title">Settings</h5>
-            <p class="card-text">Adjust the account to your needs.</p>
+            <h5 class="card-title" :class="themeText">Settings</h5>
+            <p class="card-text" :class="themeText">Adjust the account to your needs.</p>
           </div>
           <ul class="list-group list-group-flush">
-            <li class="list-group-item">
+            <li class="list-group-item" :class="themeCard">
               <div class="row">
-                <div class="col-6">
+                <div class="col-6" :class="themeText">
                   Safe search
                 </div>
                 <div class="col-6">
@@ -22,9 +22,9 @@
                 </div>
               </div>
             </li>
-            <li class="list-group-item">
+            <li class="list-group-item" :class="themeCard">
               <div class="row">
-                <div class="col-6">
+                <div class="col-6" :class="themeText">
                   Dark mode
                 </div>
                 <div class="col-6">
@@ -65,7 +65,24 @@ export default {
       user_id: 0
     }
   },
+  computed: {
+    themeCard() {
+      return {
+        'bg-dark-middle': this.darkTheme(),
+        'bg-bluegray-50': !this.darkTheme()
+      }
+    },
+    themeText() {
+      return {
+        'text-light': this.darkTheme(),
+        'text-light-black': !this.darkTheme()
+      }
+    },
+  },
   methods: {
+    darkTheme() {
+      return config.isDarkModeOn()
+    },
     updateSettings(my_settings) {
       this.my_settings = my_settings == null ? [] : my_settings
       this.toggle = this.my_settings.includes('toggle') ? true : false
