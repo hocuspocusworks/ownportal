776a66927a7939e842ad87fa361053602b48a221
Jean Petric <petricomp@gmail.com>
2022-12-23 00:17:36 +0100

    FE Password change feature

--- webapp/src/views/Setting.vue
+++ webapp/src/views/Setting.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="container">
+    <div v-if="passwordChanged" class="alert alert-success alert-dismissible fade show" role="alert">
+      <strong>Password successfully changed.</strong>
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" @click="hidePasswordChanged"></button>
+    </div>
+
+    <div v-if="passwordNotChanged" class="alert alert-danger alert-dismissible fade show" role="alert">
+      <strong>Password did not change due to: {{ passwordError }}.</strong>
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close" @click="hidePasswordChanged"></button>
+    </div>
+
     <div class="row pt-2 p-lg-4">
       <div class="col-lg-2"></div>
       <div class="col-12 col-lg-8">
@@ -55,11 +65,50 @@
     </div>
 
     <div class="row pt-4">
+      <div class="col-3"></div>
+      <div class="col">
+        <button type="button" class="btn btn-outline-secondary" data-bs-toggle="modal" data-bs-target="#passwordModal">
+          Change password
+        </button>
+      </div>
       <div class="col">
         <button type="button" class="btn btn-outline-danger" @click="remove()">Delete account</button>
       </div>
+      <div class="col-3"></div>
     </div>
 
+  </div>
+
+  <!-- Modal -->
+  <div id="passwordModal" class="modal" tabindex="-1">
+    <div class="modal-dialog">
+      <div class="modal-content text-dark">
+        <div class="modal-header">
+          <h5 class="modal-title">Change password</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <form>
+            <div class="row mb-3">
+              <label for="inputPassword" class="col-sm-6 col-form-label">New password</label>
+              <div class="col-sm-6">
+                <input type="password" class="form-control" id="inputPassword" v-model="new_password">
+              </div>
+            </div>
+            <div class="row mb-3">
+              <label for="repeatPassword" class="col-sm-6 col-form-label">Repeat password</label>
+              <div class="col-sm-6">
+                <input type="password" class="form-control" id="repeatPassword" v-model="repeated_password">
+              </div>
+            </div>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary" data-bs-dismiss="modal" @click="changePassword">Apply</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -76,7 +125,12 @@ export default {
       safe: false,
       dark: false,
       summary: false,
-      user_id: 0
+      user_id: 0,
+      new_password: null,
+      repeated_password: null,
+      passwordChanged: false,
+      passwordNotChanged: false,
+      passwordError: ''
     }
   },
   computed: {
@@ -96,6 +150,30 @@ export default {
   methods: {
     darkTheme() {
       return config.isDarkModeOn()
+    },
+    changePassword() {
+      if (this.new_password != null &&
+        this.new_password == this.repeated_password) {
+        let url = config.gateway + config.getPath('users') + '/' + config.userId()
+        let payload = { 'user': { 'password': this.new_password } }
+        axios.patch(url, payload, { headers: config.authorisationHeader() })
+          .then(response => {
+            if (response.status === 200) {
+              this.passwordChanged = true
+              this.passwordNotChanged = false
+            }
+          }).catch(ex => {
+            this.passwordNotChanged = true
+            this.passwordChanged = false
+            this.passwordError = ex.response.data.errors
+          })
+      }
+      this.new_password = null
+      this.repeated_password = null
+    },
+    hidePasswordChanged() {
+      this.passwordChanged = false
+      this.passwordNotChanged = false
     },
     updateSettings(my_settings) {
       this.my_settings = my_settings == null ? [] : my_settings
