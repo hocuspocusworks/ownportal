faea72d0c8897e7a8ffa1b8ca32d715353898ede
Jean Petric <petricomp@gmail.com>
2023-03-03 22:47:51 +0100

    Simplify registration confirmation email

--- api/app/jobs/send_registration_email.rb
+++ api/app/jobs/send_registration_email.rb
@@ -18,21 +18,7 @@ class SendRegistrationEmail < ApplicationJob
       sender: { name: 'ownportal', email: 'admin@ownportal.net' },
       to: [{ email: @user.email, name: @user.email }],
       subject: 'Registration confirmation',
-      htmlContent: '<h1>Welcome to ownportal!</h1>' \
-                   '<p>ownportal is a free, simple and fast RSS feed reader.</p>' \
-                   '<p>To get started, we suggest you to read the following articles:<p>' \
-                   '<ul>' \
-                   "<li><a href='#{effective_ownportal_link}'>Effective use of ownportal</a></li>" \
-                   "<li><a href='#{notifications_link}'>Use of notifications</a></li>" \
-                   "<li><a href='#{podcasts_link}'>Use built-in podcast player</a></li>" \
-                   "<li><a href='#{history_link}'>Keep your browsing history</a></li>" \
-                   '</ul>' \
-                   '<br>' \
-                   '<p>We hope you will enjoy using ownportal. For any questions email us at admin@ownportal.net.</p>' \
-                   '<p>Also, for all news follow our ownportal RSS blog which is added to your groups by default.</p>' \
-                   '<p>Happy reading!</p>' \
-                   '<br>' \
-                   '<h5>The ownportal team.</h5>' \
+      htmlContent: '<h1>Welcome to ownportal!</h1>'
     }.to_json
   end
 
