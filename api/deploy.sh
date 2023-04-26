84ae79cea6ace7240731b075cda303eb61d8669b
Jean Petric <petricomp@gmail.com>
2023-04-26 20:21:09 +0200

    Deploy script

---
+++ api/deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SIDEKIQ_PID=`ps aux | grep sidekiq`
+
+while read -r pid; do
+        echo "KILLING SIDEKIQ $pid"
+        kill -9 $pid
+done <<< "$SIDEKIQ_PID"
+
+WORKER_PIDS=`ps aux | grep puma | awk '{print $2}'`
+
+while read -r pid; do
+	echo "KILLING PUMA $pid"
+	kill -9 $pid
+done <<< "$WORKER_PIDS"
+
+git pull origin dev --no-edit
+
+bundle exec rails db:migrate RAILS_ENV=production
+bundle exec rails s -e production &
+sleep 5
+bundle exec sidekiq -e production &
+
