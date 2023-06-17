206f4c7a4708dfc489693b7e597e1c0721479040
Jean Petric <petricomp@gmail.com>
2023-06-17 18:55:52 +0200

    Return no content if wrong url

--- api/app/controllers/api/explores_controller.rb
+++ api/app/controllers/api/explores_controller.rb
@@ -16,7 +16,7 @@ module Api
     def rss
       rss = Api::Services::RssFinder.new(user_params[:url], current_user).call
 
-      return if rss == 'wrong url'
+      head :no_content if rss == 'wrong url'
 
       FetchFeedJob.perform_later(rss.id)
       render_json rss
